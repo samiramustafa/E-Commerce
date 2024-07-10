16a19f86fb65a075d86e39a1382a9e4cdfd4ee39
samiramustafa <samiramustafa138@gmail.com>
2024-07-11 01:43:30 +0300

    Add Swagger documentation

--- product/models.py
+++ product/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# نموذج Category للفئات الأساسية
+# Category model
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
-# نموذج Subcategory للفئات الفرعية
+#  Subcategory model
 class Subcategory(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
     name = models.CharField(max_length=255)
@@ -17,22 +17,22 @@ class Subcategory(models.Model):
     def __str__(self):
         return self.name
 
-# نموذج Product للمنتجات
+#  Product model
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    bestseller = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    bestseller = models.BooleanField(default=False)   
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE, related_name='products')
-    user=models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    added_by = models.ForeignKey(User,  null=True,on_delete=models.CASCADE, related_name='products_added')
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
 
-# نموذج ProductImage لصور المنتجات
+#  ProductImage  model
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='product_images/')
--- project/settings.py
+++ project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
      'rest_framework_simplejwt',
      'product',
      'corsheaders',
+     'drf_yasg',
     
 ]
 
